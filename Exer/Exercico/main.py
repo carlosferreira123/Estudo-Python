f14e41016985fb1b5da331adf637b032740d5e87
carlosferreira123 <122993323+carlosferreira123@users.noreply.github.com>
2024-06-19 18:36:56 -0300

    Atualização

--- Exer/Exercico/main.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual o seu nome ? ')
-idade = float(input('Qual é sua idade ?') )
-peso = float(input('Qual é o seu pedo ?'))
-
-print (f'Seu nome é {nome} , sua idade é {idade} seu peso é {peso}.')
