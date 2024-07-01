bd74ea62b09a199a15bd1005eff5d4dcd30daf23
carlosferreira123 <122993323+carlosferreira123@users.noreply.github.com>
2024-07-01 18:07:27 -0300

    Atualização

---
+++ Aula 11/exe36.py
@@ -0,0 +1,9 @@
+casa = float(input("Qual o valor da casa ? "))
+salario = float(input("Qual é o seu salario ? "))
+anos = int(input("Quantos anos você vai pagar ? "))
+
+prestação = casa / (anos * 12)
+
+if prestação > 30% salario:
+   print(f"O emprestimo é negado")
+elif: prestação    
